958c0f035bffce48155c4dba98bc8eb39d3b22ed
hal-9001-v <57011035+hal-9001-v@users.noreply.github.com>
2021-03-19 19:45:49 +0100

    Input Basics

---
+++ Assets/Scripts/Input/InputDistributor.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputDistributor : MonoBehaviour
+{
+    NormalInput inputMap;
+
+    static InputDistributor instance;
+
+
+    private void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+
+            inputMap = new NormalInput();
+
+            setInput();
+
+            inputMap.Enable();
+        }
+
+    }
+
+    private void OnDisable()
+    {
+        if (inputMap != null)
+            inputMap.Disable();
+    }
+
+    private void OnEnable()
+    {
+        if (inputMap != null)
+            inputMap.Enable();
+    }
+
+    void setInput()
+    {
+
+        if (inputMap != null)
+        {
+            foreach (InputComponent components in FindObjectsOfType<InputComponent>())
+            {
+                components.setInput(inputMap);
+            }
+        }
+        else
+        {
+            Debug.LogWarning("No NormalInput in Input");
+        }
+
+
+
+    }
+}
---
+++ Assets/Scripts/Input/Movement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : InputComponent
+{
+    public override void setInput(NormalInput inputs)
+    {
+        inputs.Map.Movement.performed += ctx =>
+        {
+            Vector3 v = transform.position;
+
+            var aux = ctx.ReadValue<Vector2>();
+
+            v.x += aux.x;
+            v.y += aux.y;
+
+            transform.position = v;
+        };
+
+
+    }
+
+}
